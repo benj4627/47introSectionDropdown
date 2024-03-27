cf5153ca6523960710270fa672bd82ee50f78b6c
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-27 09:40:52 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 let headerEl = document.querySelectorAll('.dropdown');
 let dropdownLiEl = document.querySelectorAll('.hidden')
 
-//tilføj eventlistenter, hover/mouse over? med forEach
+//tilføj eventlistenter, hover/mouseOver/mouseEnter? med forEach
 
-//classListAdd der viser dem med height og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
+//classListAdd eller toggle som tildeler .active og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
 
-//classListRemove der ved mouseleave der kollapser dem igen
+//classListToggle eller remove .active der ved mouseleave der kollapser dem igen
 
 
