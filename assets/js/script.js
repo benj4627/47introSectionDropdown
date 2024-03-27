9a5a7170354edccb8fcf04d54e2947ac8ce90826
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-27 09:39:25 +0100

    updated css and wrote pseudocode js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//fang LIs i header og andre relevante elementer
+let headerEl = document.querySelectorAll('.dropdown');
+let dropdownLiEl = document.querySelectorAll('.hidden')
+
+//tilføj eventlistenter, hover/mouse over? med forEach
+
+//classListAdd der viser dem med height og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
+
+//classListRemove der ved mouseleave der kollapser dem igen
+
+
