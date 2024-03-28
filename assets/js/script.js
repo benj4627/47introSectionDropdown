cced75cedba8071b0eb20795ec6db5b828bb34ab
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-28 08:30:00 +0100

    finished JS For dekstop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 //fang LIs i header og andre relevante elementer
 let headerEl = document.querySelectorAll('.dropdown');
-let dropdownLiEl = document.querySelectorAll('.hidden')
+let hiddenEl = document.querySelectorAll('.hidden');
 
 //tilføj eventlistenter, hover/mouseOver/mouseEnter? med forEach
-
 //classListAdd eller toggle som tildeler .active og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
+headerEl.forEach(headerLi => {
+    headerLi.addEventListener('mouseover', () => {
+        hiddenEl.forEach(hidden => {
+        hidden.classList.toggle('active');
+        });
+    });
+    headerLi.addEventListener('mouseleave', () => {
+        hiddenEl.classList.remove('')
+    })
+});
 
-//classListToggle eller remove .active der ved mouseleave der kollapser dem igen
 
-
-//lav html elementer til burger menu og lav den på samme måde som tidligere notifikations card fra timen. 
 
