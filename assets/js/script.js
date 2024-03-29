41deaa4504bc7220232f8c97b90a221597b04726
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-29 07:17:04 +0100

    updated css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
 //fang LIs i header og andre relevante elementer
+//tilføj eventlistenter, hover/mouseOver/mouseEnter? med forEach
+//classListAdd eller toggle som tildeler .active og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
 let headerEl = document.querySelectorAll('.dropdown');
 let hiddenEl = document.querySelectorAll('.hidden');
 
-//tilføj eventlistenter, hover/mouseOver/mouseEnter? med forEach
-//classListAdd eller toggle som tildeler .active og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
 headerEl.forEach(headerLi => {
-    headerLi.addEventListener('mouseover', () => {
+    headerLi.addEventListener('click', () => {
         hiddenEl.forEach(hidden => {
         hidden.classList.toggle('active');
         });
     });
-    headerLi.addEventListener('mouseleave', () => {
-        hiddenEl.classList.remove('')
-    })
+    // headerLi.addEventListener('click', () => {
+    //     hiddenEl.classList.remove('')
+    // })
 });
-
 
 //fang navigationen, class list add en toggle class, så den flyver ind fra højre. Sæt class i CSS til at have height og width 0. Tjek eventuelt popup fra tidligere opgave omkring notifikation
 
@@ -26,7 +25,6 @@ let toggleEl = document.querySelector('.toggle');
 burgerEl.addEventListener('click', () => {
     toggleEl.classList.toggle('toggle');
     toggleEl.classList.toggle('burgerMenuItems');
-
 });
 
 //classlist toggle mellem toggle og burgerMenuItems
