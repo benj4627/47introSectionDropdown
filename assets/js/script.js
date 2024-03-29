1675ddd3f2c88d5e4560a0cadb2c7031bdc398be
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-29 07:36:36 +0100

    updated CSS and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 //fang LIs i header og andre relevante elementer
 //tilføj eventlistenter, hover/mouseOver/mouseEnter? med forEach
 //classListAdd eller toggle som tildeler .active og noget med position? relative/absolute og Z-index, så det bliver vist over andre elementer. 
-let headerEl = document.querySelectorAll('.dropdown');
-let hiddenEl = document.querySelectorAll('.hidden');
+    let headerEl = document.querySelectorAll('.dropdown');
+    let hiddenEl = document.querySelectorAll('.hidden');
+    let arrowEl = document.querySelector('#arrowDropdown')
 
-headerEl.forEach(headerLi => {
-    headerLi.addEventListener('click', () => {
-        hiddenEl.forEach(hidden => {
-        hidden.classList.toggle('active');
+
+    headerEl.forEach(headerLi => {
+        headerLi.addEventListener('click', () => {
+            hiddenEl.forEach(hidden => {
+                hidden.classList.toggle('active');
+                arrowEl.classList.toggle('animArrow');
+            });
         });
     });
-    // headerLi.addEventListener('click', () => {
-    //     hiddenEl.classList.remove('')
-    // })
-});
 
 //fang navigationen, class list add en toggle class, så den flyver ind fra højre. Sæt class i CSS til at have height og width 0. Tjek eventuelt popup fra tidligere opgave omkring notifikation
 
